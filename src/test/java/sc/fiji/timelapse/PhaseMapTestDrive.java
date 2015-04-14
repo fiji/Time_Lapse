dafde30123fa3e5a9af377a54ae0a1228554f1f8
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-04-14 16:59:22 +0200

    Phase Map (test drive): show profile stack and profile map by default
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src/test/java/sc/fiji/timelapse/PhaseMapTestDrive.java
+++ src/test/java/sc/fiji/timelapse/PhaseMapTestDrive.java
@@ -5,6 +5,6 @@ import fiji.Debug;
 public class PhaseMapTestDrive {
 	public static void main(final String... args) {
 		final String testImage = PhaseMapTestDrive.class.getResource("/kymograph_140816_yfp_s0017.tif").getPath();
-		Debug.runFilter(testImage, "Phase Map", " octave_number=3 ", false);
+		Debug.runFilter(testImage, "Phase Map", " octave_number=3 show_profile_stack show_phase_profile_map ", false);
 	}
 }
